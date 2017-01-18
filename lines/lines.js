fafeaa4620ee3e0a6a38e2152f04772529eee1ce
Ankur Pandey <ankurkumarpandey@gmail.com>
2017-01-18 18:45:44 +0530

    Added feature with tests, check which side of the line is the point on

--- lines/lines.js
+++ lines/lines.js
@@ -1,11 +1,36 @@
 var findAngle = function(point1, point2) {
   // this function calculates the angle created by an imaginary line segment drawn from point1 to point2, assuming the base of x-axis is 0
   // console.log(point1);
+
   var angle = Math.atan((point2.y - point1.x) / (point2.x - point1.x));
 
   return angle;
 };
 
+var getAngleBetweenTwoLines = function(point1, point2, point3, point4) {
+  // Calculates the angle between imaginary lines drawn between point1 to point2 and point3 to point4
+  var angle1 = findAngle(point1, point2);
+  var angle2 = findAngle(point3, point4);
+
+  return angle2 - angle1;
+};
+
+var checkSide = function(point1, point2, pointToCheck) {
+  // Accepts two points and draws and imaginary line segment going from point1 to point2
+  // Then tells which side of the line is this point on
+
+  var requiredAngle = getAngleBetweenTwoLines(point1, point2, point2, pointToCheck);
+
+  if (requiredAngle < 0) {
+    return 'right'
+  } else if (requiredAngle == 0) {
+    return 'online';
+  } else {
+    return 'left';
+  }
+};
+
 module.exports = {};
 module.exports.lines = {};
 module.exports.lines.findAngle = findAngle;
+module.exports.lines.checkSide = checkSide;
--- specs/lines/lines.js
+++ specs/lines/lines.js
@@ -17,3 +17,46 @@ describe('Checking if the get angle function works or not', function() {
     expect(gotAngle).to.equal(Math.PI / 4);
   });
 });
+
+
+describe('Checking if the  direction of point function works or not', function() {
+  it('Should say right when the point is on right', function() {
+    var gotSideOfLine = lines.checkSide({
+      x: 2,
+      y: 2
+    }, {
+      x: 3,
+      y: 3
+    }, {
+      x: 7,
+      y: 5
+    });
+    expect(gotSideOfLine).to.equal('right');
+  });
+  it('Should say left when the point is on left', function() {
+    var gotSideOfLine = lines.checkSide({
+      x: 2,
+      y: 2
+    }, {
+      x: 3,
+      y: 3
+    }, {
+      x: 5,
+      y: 7
+    });
+    expect(gotSideOfLine).to.equal('left');
+  });
+  it('Should say on the line when on the line', function() {
+    var gotSideOfLine = lines.checkSide({
+      x: 2,
+      y: 2
+    }, {
+      x: 3,
+      y: 3
+    }, {
+      x: 5,
+      y: 5
+    });
+    expect(gotSideOfLine).to.equal('online');
+  });
+});
